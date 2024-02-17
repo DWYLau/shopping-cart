d87207745405af8e72ee700285855e9011c75405
Derek <dereklau15@gmail.com>
2024-02-17 19:43:41 +0900

    Create, style header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,41 @@
-function Header() {}
+import { Link } from "react-router-dom"
+import styles from "./Header.module.css"
+import Search from "../../assets/icons/search.svg"
+import Shopping from "../../assets/icons/shopping-bag.svg"
+
+function Header() {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>SHOPPIT</h1>
+        <div className={styles["link-container"]}>
+          <Link className={styles.link} to='home'>
+            Home
+          </Link>
+          <Link className={styles.link} to='products'>
+            Products
+          </Link>
+        </div>
+        <div className={styles["search-field"]}>
+          <input
+            className={styles.input}
+            type='search'
+            placeholder='Search for items'
+          />
+          <img className={styles["search-icon"]} src={Search} alt='Search' />
+        </div>
+        <img
+          className={styles["shopping-icon"]}
+          src={Shopping}
+          alt='Shopping Bag'
+        />
+      </header>
+      <header className={styles["second-header"]}>
+        <div className={styles.banner}>50% Global Discount</div>
+        <div className={styles.banner}>Free Worldwide Delivery</div>
+      </header>
+    </>
+  )
+}
 
 export default Header
